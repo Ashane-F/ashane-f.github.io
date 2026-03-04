b2500ab81af80218e569efe2791216cc65899a00
Ashane Fernando <103853683+Ashane-F@users.noreply.github.com>
2026-03-04 10:07:33 +0530

    Update footer.js

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ const lastPushedDate = "February 13, 2026";
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("last-pushed").textContent =
-        "Last pushed to GitHub: " + lastPushedDate;
+        "    Last pushed to GitHub: " + lastPushedDate;
 
     document.getElementById("year").textContent =
         new Date().getFullYear();
