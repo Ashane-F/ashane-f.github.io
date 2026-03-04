e6623bfb53ed02f085d8cfa28320945c714478ab
Ashane Fernando <103853683+Ashane-F@users.noreply.github.com>
2026-03-04 10:38:15 +0530

    Update footer.js

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ const lastPushedDate = "March 04, 2026";
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("last-pushed").textContent =
-        "Last updated: " + lastPushedDate;
+        "Last Updated: " + lastPushedDate;
 
     document.getElementById("year").textContent =
         new Date().getFullYear();
